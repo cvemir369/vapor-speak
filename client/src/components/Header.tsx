2e3c85c684a7b3ed2be053ef6bb615ec898483a2
cvemir369 <cvemir369@gmail.com>
2025-08-12 00:18:15 +0200

    logo favico added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,13 +2,23 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function Header() {
   const pathname = usePathname();
 
   return (
     <header className="flex items-center justify-center md:items-center md:justify-between p-3 bg-neutral-800">
-      <Link href="/" className="font-bold text-2xl hover:text-gray-500">
+      <Link
+        href="/"
+        className="font-bold text-2xl hover:text-gray-500 flex items-center gap-2"
+      >
+        <Image
+          src="/favicon-32x32.png"
+          alt="Vapor Speak Logo"
+          width={32}
+          height={32}
+        />
         Vapor Speak
       </Link>
       {/* Navigation - Hidden on mobile, visible on desktop */}
