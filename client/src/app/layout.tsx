75e9801e2648f03c805d37a0d9b956cc3459b733
cvemir369 <cvemir369@gmail.com>
2025-08-12 11:57:38 +0200

    links fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -15,15 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="preload"
-          href="/_next/static/media/8ee3a1ba4ed5baee-s.p.be19f591.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </head>
       <body className="antialiased min-h-screen bg-neutral-900 text-neutral-50">
         <Header />
         <div className="flex flex-col items-center justify-center min-h-screen">
